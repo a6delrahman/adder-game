c133df543de7568305bf9b2217abc90fd587f48b
Abdelrahman <abdelrahman.abdelwahed@students.ffhs.ch>
2024-12-28 02:53:52 +0100

    minor update

--- frontend/src/components/pages/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage.jsx
@@ -84,7 +84,7 @@ const ProfilePage = () => {
     return (
         <div className="profile-page">
             <h1>User Profile</h1>
-            <p>Here, you can update your personal information by clicking Change or Update Email and make changes. If you ever wish to deactivate your account, use the Delete Account option at the bottom. Be cautious, as this action is permanent.</p>
+            <p>Here, you can update your personal information by clicking Change, Update Email or change password to make changes. If you ever wish to deactivate your account, use the Delete Account option at the bottom. Be cautious, as this action is permanent.</p>
             {message && <p>{message}</p>}
             {user ? (
                 <div className='profileFields'>
