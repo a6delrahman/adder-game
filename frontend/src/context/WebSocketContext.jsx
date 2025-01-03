72a715fda0a2c32c933bbb7db634f8278cd5e684
Abdelrahman <abdelrahman.abdelwahed@students.ffhs.ch>
2025-01-03 16:12:48 +0100

    wss to ws

--- frontend/src/context/WebSocketContext.jsx
+++ frontend/src/context/WebSocketContext.jsx
@@ -158,7 +158,7 @@ export const WebSocketProvider = ({children}) => {
   });
 
   useEffect(() => {
-    const socket = new WebSocket('wss://adder-backend.azurewebsites.net');
+    const socket = new WebSocket('ws://adder-backend.azurewebsites.net');
 
     socket.onopen = () => {
       console.log('WebSocket connected');
